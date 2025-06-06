de2170fa0c3f15e3a43dc7f61751ee3057c1c3a7
Faizan Shaikh <faizanshaikh008007@gmail.com>
2025-06-06 21:07:22 +0530

    Added Streaming COmponent

--- server.js
+++ server.js
@@ -1,3 +1,73 @@
+
+// const express = require('express');
+// const http = require('http');
+// const {Server}= require('socket.io');
+// const path = require('path');
+// const cors=require('cors');
+
+
+// const app = express();
+
+// const server = http.createServer(app);
+
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST'],
+//     credentials: true
+//   }
+// });
+
+// app.use(cors({
+//   origin: 'http://localhost:5173', // Allow all origins
+//   credentials: true , // Allow credentials 
+//   transports: ['websocket', 'polling'] 
+// }));
+// let broadcasterSocketId = null;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'broadcaster.html'));
+// });
+
+// app.get('/viewer', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'viewer.html'));
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('New client connected:', socket.id);
+
+//   socket.on('broadcaster', () => {
+//     broadcasterSocketId = socket.id;
+//     console.log('Broadcaster ready:', broadcasterSocketId);
+//   });
+
+//   socket.on('viewer', () => {
+//     if (broadcasterSocketId) {
+//       io.to(broadcasterSocketId).emit('viewer:request', socket.id);
+//     }
+//   });
+
+//   socket.on('signal', ({ target, signal }) => {
+//     io.to(target).emit('signal', { signal, sender: socket.id });
+//   });
+
+//   socket.on('chat:message', (msg) => {
+//     if (broadcasterSocketId) {
+//       io.to(broadcasterSocketId).emit('chat:message', msg);
+//     }
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('Disconnected:', socket.id);
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server running on http://localhost:3000');
+// });
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -37,6 +107,12 @@ io.on('connection', (socket) => {
 
   socket.on('signal', ({ target, signal }) => {
     io.to(target).emit('signal', { signal, sender: socket.id });
+  });
+  socket.on('chat:message', (msg) => {
+    if (broadcasterSocketId) {
+      io.to(broadcasterSocketId).emit('chat:message', msg);
+      console.log(`Message from ${socket.id} to broadcaster:`, msg);
+    }
   });
 
   socket.on('disconnect', () => {
